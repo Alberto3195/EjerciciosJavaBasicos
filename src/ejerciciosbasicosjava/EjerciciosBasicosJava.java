f3c38daaa9b10a205e653cde69962f4c8884c661
Alberto <Alberto@LAPTOP-RD4DMT3L>
2019-11-28 18:18:20 +0100

    Con el tercer ejercicio empezado

--- src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
+++ src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
@@ -7,7 +7,7 @@ package ejerciciosbasicosjava;
 public class EjerciciosBasicosJava {
 
     public boolean fiestaArdillas(int numBellotas, boolean finDeSemana) {
-        if (finDeSemana){
+        if (finDeSemana) {
             return true;
         }
         if (numBellotas >= 40 && numBellotas <= 60) {
@@ -15,25 +15,43 @@ public class EjerciciosBasicosJava {
         }
         return false;
     }
-    
-    public int multa (int velocidad , boolean cumple) {
-        return 0;
-        
+
+    public int multa(int velocidad, boolean cumple) {
+        if (cumple) { //Si es mi cumple, el guardia me quita 5 de velicidad
+            velocidad -= 5;
+        }
+        if (velocidad <= 60) {
+            return 0;
+        }
+        if (velocidad > 60 && velocidad <= 80) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    public int vanidoso(int numero, boolean multiplo) {
+        if (numero % 11 == 0 || numero % 11 == 11) {
+            return true;
+        }
+        return false;
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-EjerciciosBasicosJava ejercicios = new EjerciciosBasicosJava();
-System.out.println(ejercicios.fiestaArdillas(30, false));
-System.out.println(ejercicios.fiestaArdillas(50, false));
-System.out.print(ejercicios.fiestaArdillas(70, true));
+        EjerciciosBasicosJava ejercicios = new EjerciciosBasicosJava();
+        System.out.println(ejercicios.fiestaArdillas(30, false));
+        System.out.println(ejercicios.fiestaArdillas(50, false));
+        System.out.println(ejercicios.fiestaArdillas(70, true));
+
+        System.out.println(ejercicios.multa(60, false));
+        System.out.println(ejercicios.multa(65, false));
+        System.out.println(ejercicios.multa(65, true));
+
+        System.out.println(ejercicios.vanidoso(22, true));
+        System.out.println(ejercicios.vanidoso(23, true));
+        System.out.println(ejercicios.vanidoso(24, false));
     }
-
-    
-
-
-
-
 }
